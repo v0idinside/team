f029bcc7b87f8b6a5f26ccfd3cc1faaaced4d010
v0idinside <voidinsideteam@gmail.com>
2025-06-14 15:31:49 +0300

    форма тепер працює і ще пофіксив слайдер

--- src/components/webDev/WebDev.jsx
+++ src/components/webDev/WebDev.jsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WebDev.css';
 import telegramImg from '../../assets/telegram.png';
 import { webDevTranslations } from './translation';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const WebDev = ({ language }) => {
   const t = webDevTranslations[language];
+
+
+  const notifySuccess = (message) => toast.success(message);
+  const notifyError = (message) => toast.error(message);
+
+
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    email: '',
+  });
+
+
+  const handleChange = (e) => {
+    setFormData(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('https://formspree.io/f/xeokkqwq', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+      setFormData({ name: '', phone: '', email: '' });
+      notifySuccess(t.successMessage)
+    } else {
+      notifyError(t.errorMessage)
+    }
+  };
 
   return (
     <div className='WebDev-con' id='WebDev-con'>
       <h1 className='WebDev-title'>{t.title}</h1>
       <p className='WebDev-toOrder'>{t.description}</p>
-      
+
       <div className="WebDev-contact">
         <a className='WebDev-linkTg' href="https://t.me/Emebjxjsb" target="_blank" rel="noopener noreferrer">
           <div className="WebDev-telegram">
@@ -19,13 +60,16 @@ const WebDev = ({ language }) => {
           </div>
         </a>
 
-        <form className='WevDev-form'>
+        <form className='WevDev-form' onSubmit={handleSubmit}>
           <div className="WevDev-form-input">
             <p>{t.nameLabel}</p>
             <input 
-              type="text" 
-              placeholder={t.namePlaceholder} 
-              required 
+              type="text"
+              name="name"
+              placeholder={t.namePlaceholder}
+              required
+              value={formData.name}
+              onChange={handleChange}
             />
           </div>
 
@@ -33,27 +77,34 @@ const WebDev = ({ language }) => {
             <p>{t.phoneLabel}</p>
             <input
               type="tel"
-              id="phone"
               name="phone"
               placeholder={t.phonePlaceholder}
               pattern="^\+?[0-9]{1,4}[-\s]?[0-9]{3}[-\s]?[0-9]{3}[-\s]?[0-9]{3}$"
               required
+              value={formData.phone}
+              onChange={handleChange}
             />
           </div>
 
           <div className="WevDev-form-input">
             <p>{t.emailLabel}</p>
             <input 
-              type="email" 
-              placeholder={t.emailPlaceholder} 
-              pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-              required 
+              type="email"
+              name="email"
+              placeholder={t.emailPlaceholder}
+              required
+              value={formData.email}
+              onChange={handleChange}
             />
           </div>
 
-          <button className='WevDev-btn' type="submit">{t.submit}</button>
+          <button className='WevDev-btn' type="submit">
+            {t.submit}
+          </button>
+
         </form>
       </div>
+      <ToastContainer />
     </div>
   );
 };
--- src/components/webDev/translation.js
+++ src/components/webDev/translation.js
@@ -1,39 +1,47 @@
 export const webDevTranslations = {
-    en: {
-      title: "Complete website development",
-      description: "To place an order, write to us on Telegram or fill out the form below, and we will get in touch with you",
-      contactManager: "Click here to contact the manager",
-      nameLabel: "Enter your name",
-      namePlaceholder: "Enter your name",
-      phoneLabel: "Enter your phone number (for example: +380971234567)",
-      phonePlaceholder: "Enter your phone number",
-      emailLabel: "Enter your email address",
-      emailPlaceholder: "Enter your email",
-      submit: "Contact us"
-    },
-    ru: {
-      title: "Разработка сайта под ключ",
-      description: "Чтобы сделать заказ, напишите нам в Telegram или заполните форму ниже, и мы свяжемся с вами",
-      contactManager: "Нажмите здесь, чтобы связаться с менеджером",
-      nameLabel: "Введите ваше имя",
-      namePlaceholder: "Введите ваше имя",
-      phoneLabel: "Введите ваш номер телефона (например: +380971234567)",
-      phonePlaceholder: "Введите ваш номер телефона",
-      emailLabel: "Введите ваш email",
-      emailPlaceholder: "Введите ваш email",
-      submit: "Связаться с нами"
-    },
-    ua: {
-      title: "Розробка сайту під ключ",
-      description: "Щоб зробити замовлення, напишіть нам у Telegram або заповніть форму нижче, і ми зв’яжемося з вами",
-      contactManager: "Натисніть тут, щоб зв’язатися з менеджером",
-      nameLabel: "Введіть ваше ім’я",
-      namePlaceholder: "Введіть ваше ім’я",
-      phoneLabel: "Введіть ваш номер телефону (наприклад: +380971234567)",
-      phonePlaceholder: "Введіть ваш номер телефону",
-      emailLabel: "Введіть вашу електронну адресу",
-      emailPlaceholder: "Введіть вашу електронну адресу",
-      submit: "Зв’язатися з нами"
-    }
-  };
-  
+  en: {
+    title: "Complete website development",
+    description: "To place an order, write to us on Telegram or fill out the form below, and we will get in touch with you",
+    contactManager: "Click here to contact the manager",
+    nameLabel: "Enter your name",
+    namePlaceholder: "Enter your name",
+    phoneLabel: "Enter your phone number (for example: +380971234567)",
+    phonePlaceholder: "Enter your phone number",
+    emailLabel: "Enter your email address",
+    emailPlaceholder: "Enter your email",
+    submit: "Contact us",
+
+    successMessage: "Form sent successfully! We will contact you soon.",
+    errorMessage: "Unexpected error occurred. Please try again later.",
+  },
+  ru: {
+    title: "Разработка сайта под ключ",
+    description: "Чтобы сделать заказ, напишите нам в Telegram или заполните форму ниже, и мы свяжемся с вами",
+    contactManager: "Нажмите здесь, чтобы связаться с менеджером",
+    nameLabel: "Введите ваше имя",
+    namePlaceholder: "Введите ваше имя",
+    phoneLabel: "Введите ваш номер телефона (например: +380971234567)",
+    phonePlaceholder: "Введите ваш номер телефона",
+    emailLabel: "Введите ваш email",
+    emailPlaceholder: "Введите ваш email",
+    submit: "Связаться с нами",
+
+    successMessage: "Форма успешно отправлена! Мы свяжемся с вами в ближайшее время.",
+    errorMessage: "Произошла непредвиденная ошибка. Пожалуйста, попробуйте позже.",
+  },
+  ua: {
+    title: "Розробка сайту під ключ",
+    description: "Щоб зробити замовлення, напишіть нам у Telegram або заповніть форму нижче, і ми зв’яжемося з вами",
+    contactManager: "Натисніть тут, щоб зв’язатися з менеджером",
+    nameLabel: "Введіть ваше ім’я",
+    namePlaceholder: "Введіть ваше ім’я",
+    phoneLabel: "Введіть ваш номер телефону (наприклад: +380971234567)",
+    phonePlaceholder: "Введіть ваш номер телефону",
+    emailLabel: "Введіть вашу електронну адресу",
+    emailPlaceholder: "Введіть вашу електронну адресу",
+    submit: "Зв’язатися з нами",
+
+    successMessage: "Форма успішно надіслана! Ми скоро зв’яжемося з вами.",
+    errorMessage: "Непередбачена помилка. Будь ласка, спробуйте пізніше.",
+  }
+};
